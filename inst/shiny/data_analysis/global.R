641984e6145fc9688a9ea29d1c1155a315687e93
Matthew Denwood <md@sund.ku.dk>
2019-10-18 19:05:49 +0200

    Fix shiny app deployment

--- inst/shiny/data_analysis/global.R
+++ inst/shiny/data_analysis/global.R
@@ -1,6 +1,13 @@
 ## The required packages must be supplied like this on the second line of the file as it is used to check the packages are installed before launching:
 packages <- c("bayescount","shiny","shinythemes","rhandsontable")
 
+# Needs to be explicitly in here as library etc calls for deployment to shinyapps.io:
+library("bayescount")
+library("shiny")
+library("shinythemes")
+library("rhandsontable")
+# TODO: handle this more nicely
+
 # Load the packages:
 if(!all(sapply(packages, require, character.only=TRUE))) stop("One or more required package missing")
 
